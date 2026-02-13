273d2317c9815a839fe31fd4ef82990b8070ca6e
athmika2904 <athmika.cs24@bmsce.ac.in>
2026-02-13 18:05:42 +0530

    thoughts added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import './App.css'
 function App() {
   const [mood,setmood]=useState("");
+  const thoughts={Happy: "Today feels amazing!",
+    Nervous: "What if I mess this up?",
+    Excited: "Something big is coming!"}
   return (
     <div className="moodcard">
       <h1>Mood Reactor</h1>
@@ -9,6 +12,7 @@ function App() {
       <button onClick={()=>setmood("Nervous")}>Nervous</button>
       <button onClick={()=>setmood("Excited")}>Excited</button>
       <h2>Your current mood:{mood}</h2>
+      <p>{thoughts[mood]}</p>
     </div>
   )
 }
